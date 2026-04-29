809a2a9e5c5dbe97416df80e103ee079715ce750
Anjanikumar107 <dubeyanjani107@gmail.com>
2026-04-29 18:20:46 +0530

    2nd time update code

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,6 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\TradeController;
 use Illuminate\Support\Facades\Route;
 
+
 Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::resource('trades', TradeController::class);
